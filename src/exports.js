ee9925c97437967b6a0b0b10868ee9cc5f878cff
Toby Ferguson <toby.h.ferguson@icloud.com>
2024-03-13 12:21:30 -0700

    added exports and unit testing

---
+++ src/exports.js
@@ -0,0 +1,5 @@
+var Exports = {
+  get MembershipFunctions() {
+    return MembershipFunctions
+  }
+}
---
+++ src/tests.js
@@ -0,0 +1,48 @@
+// const ns = (() => {
+//   const unit = new bmUnitTester.Unit()
+//   const mf = new Exports.MembershipFunctions
+//   unit.section(() => {
+//     let user = mf.internal.createUserObject("J.K@santacruzcountycycling.club", "J", "K", "j.k@icloud.com", "+14083869343")
+//     unit.is({}, user)
+//   })
+//   return {unit}
+// })()
+
+function runUnitTest() {
+  const unit = new bmUnitTester.Unit()
+  const mf = Exports.MembershipFunctions
+  unit.section(() => {
+    const expected = {
+      "primaryEmail": "J.K@santacruzcountycycling.club", "name": { "givenName": "J", "familyName": "K" }, "emails": [{ "address": "j.k@icloud.com", "type": "home" }], "phones": [{ "value": "+14083869343", "type": "mobile" }], "customSchemas": { "Club_Membership": { "expires": "2024-03-13", "Join_Date": "2024-03-13" } }, "orgUnitPath": "/members", "recoveryEmail": "j.k@icloud.com", "recoveryPhone": "+14083869343"
+    }
+    let actual = mf.internal.createUserObject("J.K@santacruzcountycycling.club", "J", "K", "j.k@icloud.com", "+14083869343")
+    unit.is(expected, actual)
+  })
+  unit.section(() => {
+    unit.is('foo', 'foo', { description: 'foo is foo' })
+    unit.not('foo', 'bar', { description: 'foo is not bar' })
+  },
+    { description: 'sections can have descriptions',
+    showErrorsOnly: true }
+  )
+  unit.section(() => {
+    unit.is('foo', 'bar')
+  }, {
+    description: 'entire sections can be skipped',
+    skip: true
+  })
+}
+function runTest() {
+  const i = Exports.MembershipFunctions.internal
+  i.createUserObject("J.K@santacruzcountycycling.club", "J", "K", "j.k@icloud.com", "+14083869343")
+}
+
+function t() {
+  Logger.log(Cob.fa());
+  Logger.log(Cob.ca);
+}
+
+function t2() {
+  const mf = Exports.MFs
+  mf.internal.createUserObject()
+}
