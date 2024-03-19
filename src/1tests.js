98c49e98b3906532b2366480b724149512832ae5
Toby Ferguson <toby.h.ferguson@icloud.com>
2024-03-19 13:34:09 -0700

    implemented functional version of processPaidTransactions

--- src/1tests.js
+++ src/1tests.js
@@ -35,18 +35,19 @@ function runUnitTest() {
     const nomatch = { 'Email Address': 'foo', 'Phone Number': 'bar' }
     const member = { emails: [{ type: 'home', address: email }], phones: [{ type: 'mobile', value: phone }] }
     unit.is({ full: true }, match(txn, member))
-    unit.is({}, match(nomatch, member))
+    unit.is(false, match(nomatch, member))
     unit.is({ full: false }, match(partial1, member))
     unit.is({ full: false }, match(partial2, member))
     let members = [member]
     unit.is(member, members.find((m) => match(txn, m)))
-    unit.not(undefined, members.find((m) => match(nomatch, m)))
+    unit.is(undefined, members.find((m) => match(nomatch, m)), {  neverUndefined: false})
     unit.is(member, members.find((m) => match(partial1, m)))
     unit.is(member, members.find((m) => match(partial2, m)))
 
   },
     {
       description: "matcher tests",
+      skip: false
     })
   unit.section(() => {
     const result = { join: "", renew: "", partial: ""}
@@ -58,10 +59,16 @@ function runUnitTest() {
     let txn = { 'Email Address': email, 'Phone Number': phone, "Payable Status": "paid" }
     const partial1 = { ...txn, 'Email Address': 'foo' }
     const partial2 = { ...txn, 'Phone Number': 'foo' }
-    const nomatch = { 'Email Address': 'foo', 'Phone Number': 'bar' }
+    const nomatch = { ...txn, 'Email Address': 'foo', 'Phone Number': 'bar' }
     const member = { emails: [{ type: 'home', address: email }], phones: [{ type: 'mobile', value: phone }] }
     mf.processPaidTransactions([txn], [member], join, renew, partial)
     unit.is("renewed", result.renew)
+    mf.processPaidTransactions([partial1], [member], join, renew, partial)
+    unit.is("partial", result.partial)
+    mf.processPaidTransactions([partial2], [member], join, renew, partial)
+    unit.is("partial", result.partial)
+    mf.processPaidTransactions([nomatch], [member], join, renew, partial)
+    unit.is("joined", result.join)
   },
     { description: "processPaidTransaction tests" })
 
