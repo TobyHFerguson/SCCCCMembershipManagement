ecb59206ff4258228eb2b0c3bdea27c54c2e8074
Toby Ferguson <toby.h.ferguson@icloud.com>
2024-03-20 09:50:25 -0700

    added Renewal Tests

--- src/TransactionProcessor.js
+++ src/TransactionProcessor.js
@@ -8,16 +8,60 @@ class TransactionProcessor {
     this.directory = directory,
       this.notifier = notifier
   }
-  processTransactions(txns) {
+  processTransactions(transactions, matcher = this.matchTransactionToMember_) {
+    const txns = transactions.filter((txn) => txn['Payable Status'].startsWith('paid') && !txn.Processed)
     txns.forEach((txn) => {
-      const user = new Exports.User(txn)
-      try {
-        this.directory.addUser(user)
-        txn.Processed = new Date().toISOString().split("T")[0]
-        this.notifier.joinSuccess(txn, user)
-      } catch (err) {
-        this.notifier.joinFailure(txn, user, err)
+      let match = this.directory.members.find((m) => matcher(txn, m))
+      if (match) { //
+        if (matcher(txn, match).full) {
+          this.renew_(txn, match)
+        } else {
+          this.partial_(txn, match)
+        }
+      } else { // problem with this user. Mark transaction for followup}
+        this.join_(txn)
       }
     });
   }
+  /**
+   * @function matchTransactionToMember - return a value depending on whether the transaction matches a member
+   * @param {Transaction} transaction
+   * @param {Member} member
+   * @return {int} - -1 if partial match, 0 if no match, +1 if full match
+   */
+  matchTransactionToMember_(txn, member) {
+    let homeEmails = member.emails.filter((e) => e.type === "home")
+    let homeEmail = homeEmails[0].address
+    let mobilePhone = (member.phones === undefined ? [{ value: null }] : member.phones).filter((p) => p.type === "mobile")[0].value
+    let emailsMatch = homeEmail == txn["Email Address"]
+    let phonesMatch = mobilePhone == txn["Phone Number"]
+    let result = (emailsMatch && phonesMatch) ? { full: true } : (emailsMatch || phonesMatch) ? { full: false } : false
+    return result
+  }
+  join_(txn) {
+    const member = new Exports.User(txn)
+    try {
+      this.directory.addUser(member)
+      txn.Processed = new Date().toISOString().split("T")[0]
+      this.notifier.joinSuccess(txn, member)
+    } catch (err) {
+      this.notifier.joinFailure(txn, member, err)
+    }
+  }
+  /**
+   * Process a membership renewal. 
+   * @param (Transaction) txn the transaction causing the renewal
+   * @param (User) member the member that is renewing their membership
+   */
+  renew_(txn, member) { 
+    member.incrementExpirationDate()
+    try {
+      this.directory.updateUser(member)
+      txn.Processed = new Date()
+      this.notifier.renewalSuccess(txn, member)
+      } catch (err) {
+        this.notifier.renewalFailure(txn, member, err)
+      }
+  }
+  partial_(txn, member) { console.info("partial_")}
 }
