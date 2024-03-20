31a3d6c1e511a931f412cbfe492197a06401fb35
Toby Ferguson <toby.h.ferguson@icloud.com>
2024-03-20 05:26:23 -0700

    initial notification

--- src/Notifier.js
+++ src/Notifier.js
@@ -1,1 +1,10 @@
-class Notifier {}
+class Notifier {
+    /**
+     * Notify anyone interested that a user has been added as a consequence of the transaction
+     * @param {Transaction} txn The transaction that caused the join
+     * @param {User} user The user that was joined
+     */
+    processJoin(txn, user) {
+        Logger.log(`${user.primaryEmail} joined`)
+    }
+}
--- src/TransactionProcessor.js
+++ src/TransactionProcessor.js
@@ -1,9 +1,18 @@
 class TransactionProcessor {
-    constructor(directory) {
-        this.directory = directory
+    /**
+     * 
+     * @param {Directory} directory 
+     * @param {Notifier} notifier 
+     */
+    constructor(directory, notifier) {
+        this.directory = directory,
+            this.notifier = notifier
     }
-    processTransactions(txns){
-        txns.forEach((txn) =>
-        this.directory.addUser(new Exports.User(txn)))
+    processTransactions(txns) {
+        txns.forEach((txn) => {
+            const user = new Exports.User(txn)
+            this.directory.addUser(user)
+            this.notifier.processJoin(txn, user)
+        });
     }
 }
