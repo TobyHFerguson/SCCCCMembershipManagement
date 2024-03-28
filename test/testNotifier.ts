00d375c0997b075b874dedb44ab0fb289860b9d5
Toby Ferguson <toby.h.ferguson@icloud.com>
2024-03-28 15:03:27 -0700

    completed the success test

--- test/testNotifier.ts
+++ test/testNotifier.ts
@@ -1,56 +1,56 @@
 import chai = require('chai');
 const expect = chai.expect;
-import {Notifier} from '../src/Notifier'
+import { Notifier } from '../src/Notifier'
 import { MailAppType, DraftType, bmUnitTester } from '../src/Types';
 import { Member } from '../src/Member';
 import { Transaction } from '../src/TransactionProcessor';
 const testFixtures = (() => {
-    const sendMail:MailAppType = {
-      sendEmail(recipient, subject, text, options){
-        console.log(`To: ${recipient}`)
-        console.log(`From: ${options.from}`)
-        console.log(`Reply-to: ${options.noReply}`)
-        console.log(`subject: ${subject}`)
-        console.log(`html: ${options.htmlBody}`),
-          console.log(`text: ${text}`)
-        return this
-      },
-      getDrafts(){ return new Array<DraftType>() }
-    }
-    const txn1 =new Transaction("J", "K", "j.k@icloud.com", "+14083869343","paid","CC-TF-RNB6")
-    return {
-      subject_lines: {
-        joinSuccessSubject: "Thanks for joining SCCCC",
-        joinFailureSubject: "Join Problem",
-        renewalSuccessSubject: "Thanks for renewing your SCCCC membership",
-        renewalFailureSubject: "Renew problem",
-        ambiguousSubject: "Ambiguous transaction",
-        expiryNotificationSubject: "Your membership will expire in {{N}} days",
-        expirationSubject: "Your membership has expired"
-      },
-      txn1,
-      member1: new Member(txn1, "/test", "@a.com"),
-      error: new Error("this is the error message"),
-      sendMail: sendMail
-    }
-  })()
+  const sendMail: MailAppType = {
+    sendEmail(recipient, subject, text, options) {
+      console.log(`To: ${recipient}`)
+      console.log(`From: ${options.from}`)
+      console.log(`Reply-to: ${options.noReply}`)
+      console.log(`subject: ${subject}`)
+      console.log(`html: ${options.htmlBody}`),
+        console.log(`text: ${text}`)
+      return this
+    },
+    getDrafts() { return new Array<DraftType>() }
+  }
+  const txn1 = new Transaction("J", "K", "j.k@icloud.com", "+14083869343", "paid", "CC-TF-RNB6")
+  return {
+    subject_lines: {
+      joinSuccessSubject: "Thanks for joining SCCCC",
+      joinFailureSubject: "Join Problem",
+      renewalSuccessSubject: "Thanks for renewing your SCCCC membership",
+      renewalFailureSubject: "Renew problem",
+      ambiguousSubject: "Ambiguous transaction",
+      expiryNotificationSubject: "Your membership will expire in {{N}} days",
+      expirationSubject: "Your membership has expired"
+    },
+    txn1,
+    member1: new Member(txn1, "/test", "@a.com"),
+    error: new Error("this is the error message"),
+    sendMail: sendMail
+  }
+})()
 describe('Notifier tests', () => {
-    it('should log a success', () => {
-        const notifier = new Notifier()
-        notifier.joinSuccess(testFixtures.txn1, testFixtures.member1)
-        const actual = notifier.joinSuccessLog
-        expect(actual).to.deep.equal([{txn: testFixtures.txn1, user: testFixtures.member1}])
-    })
-    it('should log a failure', () => {
-      const notifier = new Notifier()
-      notifier.joinFailure(testFixtures.txn1, testFixtures.member1, testFixtures.error)
-      const actual = notifier.joinFailureLog
-      expect(actual).to.deep.equal([{txn: testFixtures.txn1, user: testFixtures.member1, error: testFixtures.error}])
+  it('should log a success', () => {
+    const notifier = new Notifier()
+    notifier.joinSuccess(testFixtures.txn1, testFixtures.member1)
+    const actual = notifier.joinSuccessLog
+    expect(actual).to.deep.equal([{ txn: testFixtures.txn1, user: testFixtures.member1 }])
+  })
+  it('should log a failure', () => {
+    const notifier = new Notifier()
+    notifier.joinFailure(testFixtures.txn1, testFixtures.member1, testFixtures.error)
+    const actual = notifier.joinFailureLog
+    expect(actual).to.deep.equal([{ txn: testFixtures.txn1, user: testFixtures.member1, error: testFixtures.error }])
   })
   it('should log a partial', () => {
     const notifier = new Notifier()
     notifier.partial(testFixtures.txn1, testFixtures.member1)
     const actual = notifier.partialsLog
-    expect(actual).to.deep.equal([{txn: testFixtures.txn1, user: testFixtures.member1}])
-})
+    expect(actual).to.deep.equal([{ txn: testFixtures.txn1, user: testFixtures.member1 }])
+  })
 })
