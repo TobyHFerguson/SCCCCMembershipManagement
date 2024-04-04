db479dbbc663412b2c35aff683919f6be43c44f3
Toby Ferguson <toby.h.ferguson@icloud.com>
2024-04-04 09:41:32 -0700

    First test in testEmailNotifiers working

--- src/2testEmailNotifier.ts
+++
@@ -1,6 +0,0 @@
-import { MailAppType, bmUnitTester, DraftType } from './Types'
-import { EmailNotifier, Templates } from './EmailNotifier'
-import {Member} from './Member'
-
-
-
--- test/testMockingEmail.ts
+++ test/testMockingEmail.ts
@@ -1,7 +1,7 @@
 import chai = require('chai');
-const expect = chai.expect;
 import sinon = require('sinon');
 import sinonChai = require("sinon-chai");
+const expect = chai.expect;
 chai.use(sinonChai);
 
 import { DraftType, MailAppType, SendEmailOptions } from '../src/Types';
@@ -39,7 +39,7 @@ describe("testMockingEmails Test Suite", () => {
         const mme = new MyNewNotifier(<MailAppType><unknown>mailer)
         mme.doIt();
         expect(fake.callCount).to.equal(1);
-        expect(fake.calledWithMatch("to", "from", "Body", {})).to.be.true
+        expect(fake.calledOnceWithExactly("to", "from", "body", {}), "sendMail should've been called with these arguments").to.be.true
     })
     it('should be able to fake out class methods', () => {
         // const foo = {
@@ -54,6 +54,13 @@ describe("testMockingEmails Test Suite", () => {
     
         expect(fake()).to.equal("fake value"); // returns fake value
         expect(fake.callCount).to.equal(1); // saves calling information
-    }
-    )
+    })
+    it('should be able to stub the GmailApp', () => {
+        const stub = sinon.stub(mailer);
+        stub.getDrafts.returns(new Array());
+        const uut = new MyNewNotifier(stub);
+        uut.doIt();
+        expect(stub.getDrafts).to.have.been.called
+        expect(stub.sendEmail).to.have.been.called.with.calledOnceWithExactly("to", "from", "body", { some: "foo"})
+    })
 })
