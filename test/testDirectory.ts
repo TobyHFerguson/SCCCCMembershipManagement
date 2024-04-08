33a7a04c1f084065a4b742416a1769b407fb2b59
Toby Ferguson <toby.h.ferguson@icloud.com>
2024-04-08 16:25:30 -0700

    Added Directory Tests to get more coverage

--- test/testDirectory.ts
+++ test/testDirectory.ts
@@ -1,5 +1,7 @@
 import chai = require('chai');
 import Sinon = require('sinon');
+const referee = require("@sinonjs/referee");
+const assert = referee.assert;
 import sinonChai = require('sinon-chai');
 import {
   CurrentMember,
@@ -9,7 +11,7 @@ import {
   UserType,
   UsersCollectionType,
 } from '../src/Types';
-import {Directory, Member} from '../src/Code';
+import { Directory, Member } from '../src/Code';
 const expect = chai.expect;
 chai.use(sinonChai);
 
@@ -23,7 +25,7 @@ class Users implements UsersCollectionType {
   list(optionalArgs: {}) {
     return {};
   }
-  remove() {}
+  remove() { }
   update(resource: UserType, userKey: string) {
     return resource;
   }
@@ -57,7 +59,7 @@ describe('Directory Tests', () => {
     it('should create an instance', () => {
       const users = Sinon.createStubInstance(Users);
       const members = Sinon.createStubInstance(Members);
-      const uut = new Directory(systemConfig, {Users: users, Members: members});
+      const uut = new Directory(systemConfig, { Users: users, Members: members });
       expect(uut).to.not.be.null;
       it('');
     });
@@ -66,7 +68,7 @@ describe('Directory Tests', () => {
     const users = new Users();
     const member = Sinon.createStubInstance(Member);
     const members = Sinon.createStubInstance(Members);
-    const uut = new Directory(systemConfig, {Users: users, Members: members});
+    const uut = new Directory(systemConfig, { Users: users, Members: members });
     it('should update the users includeInGlobalAddressList field ', () => {
       const currentMember: CurrentMember = {
         'First Name': 'John',
@@ -75,8 +77,8 @@ describe('Directory Tests', () => {
         'Phone Number': '1234',
         'In Directory': false,
         'Membership Type': '',
-        Joined: new Date()+'',
-        Expires: new Date()+'',
+        Joined: new Date() + '',
+        Expires: new Date() + '',
       };
       const member = new Member(currentMember, systemConfig);
       const updateStub = Sinon.stub(users, 'update');
@@ -90,15 +92,33 @@ describe('Directory Tests', () => {
     it('should return the users', () => {
       const users = Sinon.createStubInstance(Users)
       const members = Sinon.createStubInstance(Members);
-      const member  = Sinon.createStubInstance(Member);
-      users.list.returns({users: [member]})
-      const uut = new Directory(systemConfig, {Users: users, Members: members});
+      const member = Sinon.createStubInstance(Member);
+      users.list.returns({ users: [member] })
+      const uut = new Directory(systemConfig, { Users: users, Members: members });
       const actual = uut.getMembers();
       const expected = [new Member(member, systemConfig)]
       expect(actual).to.deep.equal(expected)
     })
     it('should throw the underlying error', () => {
-      
+      const users = Sinon.createStubInstance(Users)
+      const members = Sinon.createStubInstance(Members);
+      users.list.throws('this is an error')
+      const uut = new Directory(systemConfig, { Users: users, Members: members });
+      uut.getMembers();
+      expect(users.list).to.have.thrown('this is an error')
+    })
+    it('should extend the error to show the invalid org path', () => {
+      const users = Sinon.createStubInstance(Users)
+      const members = Sinon.createStubInstance(Members);
+      users.list.throws('Invalid Input: INVALID_OU_ID')
+      const uut = new Directory(systemConfig, { Users: users, Members: members });
+      try { uut.getMembers(); }
+      catch { }
+      const expected = `Sinon-provided Invalid Input: INVALID_OU_ID: "${systemConfig.orgUnitPath}"`
+      const actual = users.list.exceptions[0].message;
+      expect(actual).to.deep.equal(expected);
+      // The below doesn't work - I think the typescript definitions for sinonChai are out of date :-()
+      // expect(users.list).to.have.thrown(`Sinon-provided Invalid Input: INVALID_OU_ID: "${systemConfig.orgUnitPath}"`)
     })
   });
 });
