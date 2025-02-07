ed23107ebbc1a39874a95b7b811d7518d1266069
Toby Ferguson <toby.h.ferguson@icloud.com>
2025-02-06 16:34:28 -0800

    Part way to javascript/appscript split

---
+++ src/apps-script-wrapper.js
@@ -0,0 +1,46 @@
+// Apps Script functions
+function processTransactions() {
+    convertLinks_('Transactions');
+    const transactionsFiddler = getFiddler_('Transactions').needFormulas();
+    const transactions = getDataWithFormulas(transactionsFiddler);
+    if (transactions.length === 0) { return; }
+  
+    
+    const bulkGroupFiddler = getFiddler_('Bulk Add Groups');
+    const bulkGroupEmails = bulkGroupFiddler.getData();
+    const emailScheduleFiddler = getFiddler_('Email Schedule');
+    const emailScheduleData = emailScheduleFiddler.getData();
+    const emailScheduleFormulas = emailScheduleFiddler.getFormulaData();
+  
+    const membershipFiddler = getFiddler_('Membership');
+    const membershipData = membershipFiddler.getData();
+    const processedTransactionsFiddler = getFiddler_('Processed Transactions');
+    const processedTransactions = getDataWithFormulas(processedTransactionsFiddler);
+    const headerAndData1 = transactionsFiddler.getSheet().getRange(1, 1, 2, transactionsFiddler.getNumColumns()).getValues();
+    const headerAndData1Copy = headerAndData1.map((row, index) => index === 1 ? row.map(() => '') : row);
+  
+    const { processedRows, updatedTransactions } = processTransactionsData(transactions, membershipData, emailScheduleData, emailScheduleFormulas, bulkGroupEmails);
+    log('updatedTransactions', updatedTransactions);
+    log('updatedTransactions.length', updatedTransactions.length);
+    processedTransactions.push(...processedRows);
+  
+    bulkGroupFiddler.setData(bulkGroupEmails).dumpValues();
+    const emails = combineArrays(emailScheduleFormulas, emailScheduleData);
+    emailScheduleFiddler.setData(emails).dumpValues();
+    membershipData.sort((a, b) => a.Email.localeCompare(b.Email));
+    membershipFiddler.setData(membershipData).dumpValues();
+    transactionsFiddler.setData(updatedTransactions.length === 0 ? headerAndData1Copy : updatedTransactions).dumpValues();
+    processedTransactionsFiddler.setData(processedTransactions).dumpValues();
+  }
+
+  function sendScheduledEmailsAppScript() {
+    const emailScheduleFiddler = getFiddler_('Email Schedule');
+    const emailScheduleData = emailScheduleFiddler.getData();
+    let emailScheduleFormulas = emailScheduleFiddler.getFormulaData();
+  
+    const sentEmails = sendScheduledEmails(emailScheduleData, emailScheduleFormulas);
+    if (sentEmails.length > 0) {
+      const remainingEmails = combineArrays(emailScheduleFormulas, emailScheduleData);
+      emailScheduleFiddler.setData(remainingEmails).dumpValues();
+    }
+  }
---
+++ src/email.js
@@ -0,0 +1,100 @@
+/**
+ * Sends scheduled emails based on the email schedule data.
+ * 
+ * This function retrieves the email schedule and email log data, sorts the schedule by date,
+ * and sends emails that are scheduled to be sent on or before the current date and time.
+ * After sending an email, it logs the email and updates the schedule.
+ * 
+ * The function performs the following steps:
+ * 1. Retrieves and sorts the email schedule data.
+ * 2. Retrieves the email log data.
+ * 3. Iterates through the email schedule in reverse order.
+ * 4. Sends emails that are scheduled for the current date or earlier.
+ * 5. Logs the sent emails and updates the email schedule.
+ * 
+ * Note: The email schedule is sorted in reverse order, so the function starts processing
+ * from the end where dates are more likely to be in the past.
+ * 
+ * @function
+ */
+function sendScheduledEmails(emailScheduleData, emailScheduleFormulas) {
+    sortArraysByValue(emailScheduleData, emailScheduleFormulas, (a, b) => new Date(b["Scheduled On"]) - new Date(a["Scheduled On"]));
+    const emailsToSend = getEmailsToSend(emailScheduleData, emailScheduleFormulas);
+    return sendEmails(emailsToSend);
+}
+
+function getEmailsToSend(emailScheduleData, emailScheduleFormulas) {
+    const now = new Date().getTime();
+    const emailsToSend = [];
+    for (let i = emailScheduleData.length - 1; i >= 0; i--) {
+        const row = emailScheduleData[i];
+        if (new Date(row["Scheduled On"]).getTime() > now) {
+            break;
+        } else {
+            const Subject = expandTemplate(row.Subject, row);
+            const Body = expandTemplate(row.Body, row);
+            emailsToSend.push({ to: row.Email, subject: Subject, htmlBody: Body });
+            emailScheduleData.splice(i, 1);
+            emailScheduleFormulas.splice(i, 1);
+        }
+    }
+    return emailsToSend;
+}
+
+/**
+ * Expands a template string by replacing placeholders with corresponding values from a row object.
+ * Placeholders are in the format {key}, where key is a property name in the row object.
+ * Date fields specified in the dateFields array are converted to local date strings.
+ *
+ * @param {string} template - The template string containing placeholders.
+ * @param {Object} row - The object containing values to replace placeholders.
+ * @returns {string} - The expanded template string with placeholders replaced by corresponding values.
+ */
+function expandTemplate(template, row) {
+    const dateFields = ["Scheduled On", "Expires", "Joined", "Renewed On"]; // Add the names of fields that should be treated as dates
+    return template.replace(/{([^}]+)}/g, (_, key) => {
+        let value = row[key];
+        if (dateFields.includes(key)) {
+            value = new Date(value); // Convert to Date object if it's a date field
+            return value.toLocaleDateString(); // Convert Date objects to local date string
+        }
+        return value || "";
+    });
+};
+
+function sendEmails(emails) {
+    log(`Number of emails to be sent: ${emails.length}`);
+    const emailLogFiddler = getFiddler_('Email Log');
+    const emailLog = emailLogFiddler.getData();
+    const testEmails = PropertiesService.getScriptProperties().getProperty('testEmails');
+    if (testEmails === 'true') { // Use test path only if testEmails is explicitly set to true
+      emails.forEach(email => log(`Email not sent due to testEmails property: To=${email.to}, Subject=${email.subject}, Body=${email.body}`));
+    } else {
+      emails.forEach(email => sendSingleEmail(email, emailLog));
+      emailLogFiddler.setData(emailLog).dumpValues();
+    }
+  }
+  
+  function sendSingleEmail(email, emailLog) {
+    log(`Email Sent: :`, email);
+    try {
+      MailApp.sendEmail(email);
+    } catch (error) {
+      log(`Failed to send email to ${email.to}: ${error.message}`);
+    }
+    emailLog.push({ Timestamp: new Date(), ...email });
+  }
+  
+  
+  function testSendEmail() {
+    const recipient = "test@example.com";
+    const subject = "Test Subject";
+    const body = "This is a test email body.";
+    const options = {
+      cc: "cc@example.com",
+      bcc: "bcc@example.com",
+      attachments: [Utilities.newBlob("Attachment content", "text/plain", "test.txt")]
+    };
+    MailApp.sendEmail(recipient, subject, body, options);
+  }
+  
