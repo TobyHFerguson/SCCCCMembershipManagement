fab13ccd3cc826cbd3ddf538b6efcb0f78b6c437
Toby Ferguson <toby.h.ferguson@icloud.com>
2025-02-10 14:55:06 -0800

    Renamed all javascript functions to avoid use in UI

--- src/JavaScript/triggers.js
+++ src/JavaScript/triggers.js
@@ -25,14 +25,14 @@ if (typeof module !== 'undefined' && module.exports) {
     processPaidTransactions,
     createScheduleEntries_,
     ActionType,
-    today,
+    today: today_,
     addDaysToDate_,
     addRenewedMemberToActionSchedule_,
     calculateExpirationDate_
   };
 }
 
-function today(date = new Date()) {
+function today_(date = new Date()) {
   return new Date().setHours(12, 0, 0, 0);
 }
 
@@ -97,10 +97,10 @@ function processPaidTransactions(transactions, membershipData, actionSchedule, a
       const matchIndex = emailToMemberMap.get(txn["Email Address"]);
       if (matchIndex !== undefined) {
         const member = membershipData[matchIndex];
-        const years = getPeriod(txn);
-        renewMember(member, years, actionSchedule, actionSpecs);
+        const years = getPeriod_(txn);
+        renewMember_(member, years, actionSchedule, actionSpecs);
       } else {
-        const newMember = addNewMember(txn, actionSchedule, actionSpecs, membershipData)
+        const newMember = addNewMember_(txn, actionSchedule, actionSpecs, membershipData)
         newMembers.push({Email: newMember.Email});
       }
       txn.Timestamp = new Date();
@@ -114,21 +114,21 @@ function processPaidTransactions(transactions, membershipData, actionSchedule, a
 
 
 
-function getPeriod(txn) {
+function getPeriod_(txn) {
   if (!txn.Payment) {return 1;}
   const yearsMatch = txn.Payment.match(/(\d+)\s*year/);
   const years = yearsMatch ? parseInt(yearsMatch[1], 10) : 1;
   return years;
 }
 
-function addNewMember(txn, actionSchedule, actionSpecs, membershipData) {
+function addNewMember_(txn, actionSchedule, actionSpecs, membershipData) {
   const newMember = {
     Email: txn["Email Address"],
     First: txn["First Name"],
     Last: txn["Last Name"],
     Joined: new Date(),
-    Period: getPeriod(txn),
-    Expires: calculateExpirationDate_(getPeriod(txn)),
+    Period: getPeriod_(txn),
+    Expires: calculateExpirationDate_(getPeriod_(txn)),
     "Renewed On": '',
   };
   membershipData.push(newMember);
@@ -143,7 +143,7 @@ function addNewMemberToActionSchedule_(member, actionSchedule, actionSpecs) {
 
 function addRenewedMemberToActionSchedule_(member, actionSchedule, actionSpecs) {
   const email = member.Email;
-  removeEmails(email, actionSchedule);
+  removeEmails_(email, actionSchedule);
   const scheduleEntries = createScheduleEntries_(member, ActionType.Renew, actionSpecs);
   actionSchedule.push(...scheduleEntries);
 }
@@ -153,7 +153,7 @@ function addRenewedMemberToActionSchedule_(member, actionSchedule, actionSpecs) 
  * @param {string} email - The email address to match.
  * @param {Array} actionSchedule - The array of objects.
  */
-function removeEmails(email, actionSchedule) {
+function removeEmails_(email, actionSchedule) {
   for (let i = actionSchedule.length - 1; i >= 0; i--) {
     if (actionSchedule[i].Email === email) {
       actionSchedule.splice(i, 1);
@@ -172,7 +172,7 @@ function createScheduleEntries_(member, type, actionSpecs) {
   switch (type) {
     case ActionType.Join:
     case ActionType.Renew:
-      scheduleEntries.push({ Date: today(), Type: type, Email: member.Email });
+      scheduleEntries.push({ Date: today_(), Type: type, Email: member.Email });
     case 'Migration':
       break;
   }
@@ -180,46 +180,6 @@ function createScheduleEntries_(member, type, actionSpecs) {
   return scheduleEntries;
 }
 
-function addMemberToEmailSchedule(member, actionSchedule, actionSpecs, emailType) {
-  const email = member.Email;
-  const emailTypes = [emailType, 'Expiry 1', 'Expiry 2', 'Expiry 3', 'Expiry 4'];
-  // These formulas all use the column heading to look up the value in the Membership sheet or the Emails sheet.
-  // They are independent of row and column location, so rows and columns can be moved around without breaking the formulas.
-  const joinLookupFormula = `=INDIRECT(ADDRESS(ROW(),XMATCH("Joined",$1:$1,0)))`
-  const renewLookupFormula = `=INDIRECT(ADDRESS(ROW(),XMATCH("Renewed On",$1:$1,0)))`
-  const membershipLookupFormula = `=INDEX(Membership!$A:$ZZZ,MATCH(INDIRECT(ADDRESS(ROW(),XMATCH("Email",$1:$1,0))),Membership!$A:$A,0),XMATCH(INDEX($1:$1,COLUMN()),Membership!$1:$1,0))`
-  const emailLookupFormula = `=VLOOKUP(INDIRECT(ADDRESS(ROW(),XMATCH("Type",$1:$1,0))),Emails!$A$1:$C$7,XMATCH(INDEX($1:$1,COLUMN()),Emails!$1:$1,0),FALSE)`
-  const scheduledOnLookupFormula = `=INDIRECT(ADDRESS(ROW(),XMATCH("Expires",$1:$1,0))) + VLOOKUP(INDIRECT(ADDRESS(ROW(),XMATCH("Type",$1:$1,0))),'Expiry Schedule'!$A:$B,2,FALSE)`
-  const canonicalEntry = {
-    "Scheduled On": scheduledOnLookupFormula,
-    Type: '',
-    Email: '',
-    First: membershipLookupFormula,
-    Last: membershipLookupFormula,
-    Joined: membershipLookupFormula,
-    Period: membershipLookupFormula,
-    Expires: membershipLookupFormula,
-    "Renewed On": membershipLookupFormula,
-    Subject: emailLookupFormula,
-    Body: emailLookupFormula
-  }
-  const logMessages = [];
-  emailTypes.forEach(t => {
-    const addOn = {
-      Type: t,
-      Email: email,
-      ...(t === 'Join' ? { "Scheduled On": joinLookupFormula } : t === 'Renewal' ? { "Scheduled On": renewLookupFormula } : {}) // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax#spread_in_object_literals
-    }
-    const newEntry = {
-      ...canonicalEntry,
-      ...addOn
-    };
-    logMessages.push(`newEntry.Email: ${newEntry.Email}, newEntry.Type: ${newEntry.Type}, newEntry["Scheduled On"]: ${newEntry["Scheduled On"]}`);
-    actionSchedule.push(newEntry);
-    emailScheduleFormulas.push(newEntry);
-  });
-  log(logMessages.join('\n'));
-}
 
 /**
  * 
@@ -227,7 +187,7 @@ function addMemberToEmailSchedule(member, actionSchedule, actionSpecs, emailType
  * @param {number} period 
  * @param {} actionSchedule 
  */
-function renewMember(member, period, actionSchedule, actionSpecs) {
+function renewMember_(member, period, actionSchedule, actionSpecs) {
   member.Period = period;
   member["Renewed On"] = new Date();
   member.Expires = calculateExpirationDate_(period, member.Expires);
