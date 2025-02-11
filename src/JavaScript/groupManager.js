eeb34d64c31f5d55e20d30c5c0ae4b2596e18e0f
Toby Ferguson <toby.h.ferguson@icloud.com>
2025-02-11 10:24:59 -0800

    implemeneted javascript group management

---
+++ src/JavaScript/groupManager.js
@@ -0,0 +1,36 @@
+
+const GroupManager = (function () {
+    function addMembersToGroups(members, groupEmails, groupAddFun) {
+        processList(members, groupEmails, groupAddFun);
+    }
+
+    function removeMembersFromGroups(members, groupEmails, groupRemoveFun) {
+        processList(members, groupEmails, groupRemoveFun);
+    }
+
+    function processList(members, groups, fun) {
+        const errors = [];
+        for (let i = members.length - 1; i >= 0; i--) {
+            const member = members[i];
+            for (let group of groups) {
+                try {
+                    fun(member, group);
+                } catch (error) {
+                    errors.push(error);
+                }
+            }
+            members.splice(i, 1);
+        }
+        if (errors.length > 0) {
+            throw new AggregateError(errors, 'One or more errors occurred while processing the list');
+        }
+    }
+    return { addMembersToGroups, removeMembersFromGroups };
+})();
+
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = {
+        GroupManager
+    };
+}
