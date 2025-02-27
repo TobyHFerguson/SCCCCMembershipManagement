dc0303e8d705b5700b0cf95053b8ab105a5bbff5
Toby Ferguson <toby.h.ferguson@icloud.com>
2025-02-27 14:30:23 -0800

    Moved calculateExpirationDate from Manager to utils

--- __tests__/utils.tests.js
+++ __tests__/utils.tests.js
@@ -6,6 +6,51 @@ describe('utils', () => {
   const { getDateString, ActionType, addDaysToDate, addYearsToDate, expandTemplate, toLocaleDateString } = utils;
 
   describe('getDateString', () => {
+    describe('calculateExpirationDate', () => {
+      test('should calculate expiration date based on period in years from today if no existing expiration date is provided', () => {
+        const period = 2;
+        try {
+          const result = utils.calculateExpirationDate(period);
+        }
+        catch(e) {
+          expect(e.message).toBe('No expiration dated provided');
+        }
+      });
+  
+      test('should calculate expiration date based on period in years from existing expiration date if provided', () => {
+        const period = 3;
+        const existingExpirationDate = new Date('2030-01-01');
+        const result = utils.calculateExpirationDate(existingExpirationDate, period);
+        const expectedDate = new Date('2033-01-01');
+        expect(utils.getDateString(result)).toEqual(utils.getDateString(expectedDate));
+      });
+  
+      test('should return the greater of period added to today or the existing expiration date', () => {
+        const period = 1;
+        const existingExpirationDate = new Date();
+        existingExpirationDate.setFullYear(existingExpirationDate.getFullYear() + 2);
+        const result = utils.calculateExpirationDate(existingExpirationDate, period);
+        const expectedDate = new Date();
+        expectedDate.setFullYear(expectedDate.getFullYear() + period + 2);
+        expect(utils.getDateString(result)).toEqual(utils.getDateString(expectedDate));
+      });
+  
+      test('should handle leap years correctly', () => {
+        const period = 1;
+        const existingExpirationDate = new Date('2052-02-29');
+        const result = utils.calculateExpirationDate(existingExpirationDate, period);
+        const expectedDate = new Date('2053-03-01')
+        expect(utils.getDateString(result)).toEqual(utils.getDateString(expectedDate));
+      });
+  
+      test('should handle negative periods correctly', () => {
+        const period = -1;
+        const existingExpirationDate = new Date('2050-01-01');
+        const result = utils.calculateExpirationDate(existingExpirationDate, period);
+        const expectedDate = new Date('2049-01-01');
+        expect(utils.getDateString(result)).toEqual(utils.getDateString(expectedDate));
+      });
+    });
     test('should return a string in the format YYYY-MM-DD', () => {
       const date = new Date('2021-01-01');
       expect(getDateString(date)).toBe('2021-01-01');
