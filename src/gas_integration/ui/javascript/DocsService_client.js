6c07fc03657783c125cf8ebe29a58b32e9a20561
Toby Ferguson <toby.h.ferguson@icloud.com>
2025-05-06 18:11:13 -0700

    DocsService converted to new layout

--- src/gas_integration/ui/javascript/DocsService_client.js
+++
@@ -1,5 +0,0 @@
-function handleUserInput(form) {
-    var docURL = form.docURL;
-    var htmlContent = DocsService.convertDocToHtml(docURL);
-    DocsService.showHtmlContent(htmlContent);
-  }
---
+++ src/services/DocsService/Menu.js
@@ -0,0 +1,24 @@
+DocsService.Menu = {
+    create: () => {
+        SpreadsheetApp.getUi().createMenu('Utilities')
+            .addItem('testConvert', 'testConvert')
+            .addItem('Convert Google Doc to HTML', 'showConversionDialog')
+            .addToUi();
+    }
+
+}
+
+
+
+function showConversionDialog() {
+    DocsService.UI.showConversionDialog();
+}
+
+function testConvert() {
+    var docURL = 'https://docs.google.com/document/d/1Pi-7YpzC4WDofRYwkPiMtUjFFLkspUtszhaN9kKzwI4';
+    var htmlContent = DocsService.convertDocToHtml(docURL);
+    var htmlOutput = HtmlService.createHtmlOutput(htmlContent)
+        .setWidth(600)
+        .setHeight(400);
+    SpreadsheetApp.getUi().showModalDialog(htmlOutput, 'Converted HTML');
+}
