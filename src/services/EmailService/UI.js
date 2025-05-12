b9337598ea26a600cc6fec321ebfb70f7bc342b6
Toby Ferguson <toby.h.ferguson@icloud.com>
2025-05-12 13:04:15 -0700

    Refactor the DirectoryService to exposed the system structure Fixes #112

---
+++ src/services/EmailService/UI.js
@@ -0,0 +1,18 @@
+EmailService.UI = {
+    showEmailDialog: function () {
+        var actionSpecTypes = this._getActionSpecTypes();
+        var template = HtmlService.createTemplateFromFile('services/EmailService/EmailDialog');
+        template.actionSpecTypes = actionSpecTypes;
+        var html = template.evaluate()
+            .setWidth(400)
+            .setHeight(300);
+        SpreadsheetApp.getUi().showModalDialog(html, 'Send Test Email');
+    },
+    _getActionSpecTypes: () => {
+        var actionSpecs = Common.Data.Access.getActionSpecs();
+        const result = Object.keys(actionSpecs);
+        console.log('getActionSpecTypes', result);
+        return result;
+    }
+}
+
