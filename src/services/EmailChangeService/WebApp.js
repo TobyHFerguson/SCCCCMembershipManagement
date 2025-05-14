8243217d2aa68a3a78c532eab31a87d84f34b28b
Toby Ferguson <toby.h.ferguson@icloud.com>
2025-05-14 16:27:01 -0700

    Removed redundant doPost()

--- src/services/EmailChangeService/WebApp.js
+++ src/services/EmailChangeService/WebApp.js
@@ -4,24 +4,5 @@ EmailChangeService.WebApp = {
        const template = HtmlService.createTemplateFromFile("services/EmailChangeService/EmailChangeForm")
         template.originalEmail = userEmail; // Pass the token to the HTML template
         return template.evaluate();
-    },
-    doPost: (e, userEmail) => {
-        console.log('EmailChangeService.WebApp.doPost(e, userEmail) - e: ', e, ' userEmail: ', userEmail)
-        const action = e.parameter.action;
-        if (action === "sendVerificationCode") {
-            const token = Common.Auth.TokenManager.generateToken();
-            Common.Auth.TokenStorage.storeToken(userEmail, token);
-            const message = handleSendVerificationCode(e, userEmail)
-            const content = {
-                    message: message,
-                    emailUpdateToken: token,
-                }
-            console.log('EmailChangeService.WebApp.doPost - sendVerificationCode returning: ', content)
-            return createTextResponse(JSON.stringify(content))
-        } else if (action === "verifyAndUpdateEmail") {
-            return handleVerifyAndUpdateEmail(e);
-        } else {
-            return createTextResponse("Invalid action.", 400);
-        }
     }
 }
