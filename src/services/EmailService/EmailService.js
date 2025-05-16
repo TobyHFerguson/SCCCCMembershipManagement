8ca539db4e5a5f2b893f0960c5642a1e7ead6a1e
Toby Ferguson <toby.h.ferguson@icloud.com>
2025-05-16 09:14:51 -0700

    Updated the send test email to send an email using a member's data record

--- src/services/EmailService/EmailService.js
+++ src/services/EmailService/EmailService.js
@@ -1,28 +1,20 @@
-EmailService.sendTestEmail =  function(form) {
-    console.log('send test email', form)
-  var emailAddress = form.emailAddress;
-  console.log('selectedKeys', form.selectedKeys);
-  var selectedKeys = Array.isArray(form.selectedKeys) ? form.selectedKeys : [form.selectedKeys]; // Ensure selectedKeys is always an array
-  var actionSpecs = Common.Data.Access.getActionSpecs(); // Assuming this function returns the ActionSpecs object
+EmailService.sendTestEmail = function (form) {
+  const lookupEmail = form.lookupEmail;
+  const sendToEmail = form.sendToEmail;
+  const selectedKeys = Array.isArray(form.selectedKeys) ? form.selectedKeys : [form.selectedKeys]; // Ensure selectedKeys is always an array
 
+  const actionSpecs = Common.Data.Access.getActionSpecs(); // Assuming this function returns the ActionSpecs object
+
+  const member = Common.Data.Access.getMember(lookupEmail);
+  if (!member) {
+    console.error(`sending an email but the member ${lookupEmail} was not found`)
+    return;
+  }
   selectedKeys.forEach(function (key) {
     spec = actionSpecs[key];
     if (spec) {
-      const member = {
-        First: 'John',
-        Last: 'Doe',
-        Joined: '2020-01-01',
-        Expires: '2021-01-01',
-        Period: 1,
-        Directory: 'Yes',
-        Phone: '123-456-7890',
-        'Renewed On': '2020-12-31',
-        Migrated: '2020-01-01',
-        Email: emailAddress
-      };
-
       const message = {
-        to: member.Email,
+        to: sendToEmail,
         subject: MembershipManagement.Utils.expandTemplate(spec.Subject, member),
         htmlBody: MembershipManagement.Utils.expandTemplate(spec.Body, member)
       };
