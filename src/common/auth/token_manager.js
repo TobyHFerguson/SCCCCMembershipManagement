5de6e5d1c7839262af5d79a1b702fbbf04f7fd67
Toby Ferguson <toby.h.ferguson@icloud.com>
2025-05-28 16:47:34 -0700

    Moved the multi-use token into the Common.Auth.TokenManager namespace

--- src/common/auth/token_manager.js
+++ src/common/auth/token_manager.js
@@ -1,5 +1,26 @@
 Common.Auth.TokenManager = {
     generateToken: () => {
         return Utilities.getUuid(); // Generate a unique UUID
+    },
+    getMultiUseToken: (email) => {
+        const token = Common.Auth.TokenManager.generateToken();
+       const cache = CacheService.getScriptCache();
+        const tokenKey = `user_token_${token}`;
+        const expirationInSeconds = 900; // Example: 5 minutes
+
+        // Store the mapping between the token and the user identifier in the cache
+        cache.put(tokenKey, email, expirationInSeconds);
+        return token;
+    },
+    getEmailFromMUT: (token) => {
+        const cache = CacheService.getScriptCache();
+        const tokenKey = `user_token_${token}`;
+        const email = cache.get(tokenKey);
+        if (email) {
+            return email;
+        } else {
+            console.warning(`Token ${token} not found or expired.`);
+            return null;
+        }
     }
 }
