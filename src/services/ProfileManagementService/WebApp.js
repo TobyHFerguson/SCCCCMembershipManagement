0e3be6c58b72611678c5fa7e2d4629f80e782b43
Toby Ferguson <toby.h.ferguson@icloud.com>
2025-06-25 09:46:17 -0700

    CSS is being escaped on iPhone Fixes #129

--- src/services/ProfileManagementService/WebApp.js
+++ src/services/ProfileManagementService/WebApp.js
@@ -1,14 +1,19 @@
 ProfileManagementService.WebApp = {
     doGet: (e, userEmail) => {
         // We only get here when the previous token (and therefore email) were valid.
-       const template = HtmlService.createTemplateFromFile("services/ProfileManagementService/ProfileManagementForm")
+        const template = HtmlService.createTemplateFromFile("services/ProfileManagementService/ProfileManagementForm")
         const profile = Common.Data.Access.getMember(userEmail);
         if (!profile) {
             throw new Error(`Profile not found for email: ${userEmail}`);
         }
+        const rawCssString = HtmlService.createHtmlOutputFromFile('services/ProfileManagementService/Stylesheet').getContent();
+        console.log('rawCssString', rawCssString);
+        // Pass the raw CSS string to the main HTML template
+        template.myInlineCss = rawCssString;
         template.profile = profile; // Pass the profile to the HTML template
         template.token = Common.Auth.TokenManager.getMultiUseToken(userEmail);
         const output = template.evaluate();
+        console.log('output', output.getContent());
         return output.setTitle("SCCCC Profile Management Service");
     }
 }
